0e384c418e6c06223a65d39ce6f941a2ecac4002
abdou_hl <abderrahmane.36.hellal@gmail.com>
2022-12-13 16:44:28 +0100

    fix script

--- main.py
+++ main.py
@@ -38,7 +38,12 @@ for status in statuses:
     if status.in_reply_to_screen_name =="MakeItTshirt" or status.in_reply_to_screen_name == "abdou_hll":
         done_comments.put(status.id_str)
         continue
-    full_status = api.get_status(id=status.id_str, tweet_mode = "extended" )
+    try:
+        full_status = api.get_status(id=status.id_str, tweet_mode = "extended" )
+        full_in_replay_status = api.get_status(id=status.in_reply_to_status_id_str, tweet_mode = "extended" )
+    except:
+        done_comments.put(status.id_str)
+        continue
     if '@MakeItTshirt' not in full_status.full_text[full_status.display_text_range[0]:full_status.display_text_range[1]]:
         done_comments.put(status.id_str)
         continue
